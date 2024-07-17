df4ea814d09fd3d440cd575b063f7956ab89ee18
anahelena <anahelenaesperanca@gmail.com>
2024-07-16 21:27:06 -0300

    alterações

--- Aula-01/Exemplo1.sql
+++ Aula-01/Exemplo1.sql
@@ -1,1 +1,28 @@
-Olá mundo
+--CRIAÇÃO DO BANCO
+CREATE DATABASE IFSP;
+--USAR O BANCO
+USE IFSP;
+--CRIAR TABELA CIDADE
+CREATE TABLE cidade
+(
+    id INT AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    estado VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE cliente
+(
+    id INT AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    id_cidade INT,
+    endereco VARCHAR(100),
+    numero INT,
+    bairro VARCHAR(100),
+    data_nascimento DATETIME,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_cliente_cidade
+    FOREIGN KEY (id_cidade) REFERENCES cidade(id)
+);
